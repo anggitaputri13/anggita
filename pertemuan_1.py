b98334167f17eef102333d9ce3b15888cead7b53
anggitaputri13 <anggitaputrira065@gmail.com>
2026-08-21 16:20:47 +0700

    Add files via upload

---
+++ pertemuan_1.py
@@ -0,0 +1,17 @@
+print("hello world")
+# print("halllo dunia")
+print("Halo Apa Kabar?");  
+print("\n Selamat datang di Lab. Teknologi Informasi");  print("\n Universitas Tidar");  
+
+print("Identitas Mahasiswa")
+print("Nama\t= Anggita Putri Rahayu")
+#menampilkan nama
+print("NPM\t\t= 2605060005")
+#menampilkan NPM
+print("TTL\t\t= Temanggung, 13 Desember 2007")
+#menampilkan Tempat, Tanggal Lahir
+print("Alamat\t= Muntung, Candoroto, Temanggung, Jawa Tengah")
+#menampilkan Alamat
+print("JK\t\t= Perempuan")
+#menampilkan Jenis Kelamin
+print("Hobi\t= Scroll TikTok, Nyanyi, Rebahan, Jalan jalan")
